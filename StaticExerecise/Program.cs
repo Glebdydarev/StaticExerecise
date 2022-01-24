c4f9d5ebb6de8a4ea7e1b451dedcfebaa28e7ac7
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-24 14:26:43 -0600

    done

---
+++ StaticExerecise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExerecise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(20);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(10);
+
+
+            Console.WriteLine("After conversion");
+
+            Console.WriteLine($"celsius{celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExerecise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExerecise
+{
+    public class TempConverter
+    {
+        
+
+        public static double FahrenheitToCelsius(double farenheit)
+
+        {
+            return (farenheit - 20) / 2.0;
+        }
+
+        public static double CelsiusToFahrenheit (double celsius)
+        {
+            return (celsius * 10) / 5+ 20;
+
+        }
+
+    }
+}
